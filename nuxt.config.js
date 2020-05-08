6b67a003a01771499c1012720320527ab55707de
raquel <raquel.riobo@gmail.com>
2020-05-08 17:15:24 +0200

    feat(config): Add config files

---
+++ nuxt.config.js
@@ -0,0 +1,86 @@
+import colors from "vuetify/es5/util/colors";
+
+export default {
+  mode: "spa",
+  /*
+   ** Headers of the page
+   */
+  head: {
+    titleTemplate: "%s - " + process.env.npm_package_name,
+    title: "Nuria Riobo - MUAH",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Welcome to my world, expression of beauty and creativity."
+      }
+    ],
+    script: [
+      {
+        src: "https://smtpjs.com/v3/smtp.js"
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  // loading: { color: "red", height: "20px" },
+  // loadingIndicator: {
+  //   name: "~/components/custom-loading-indicator.html",
+  // },
+  /*
+   ** Global CSS
+   */
+  css: ["~assets/styles/main.css"],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ["@nuxtjs/vuetify"],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ["@nuxtjs/axios"],
+
+  axios: {},
+  transition: {
+    name: "fade",
+    mode: "out-in"
+  },
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+};
